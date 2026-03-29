ac0771ca0980a086720dd21e251c825485a06f50
alex vartoumain <alex.vartoumian@gmail.com>
2026-03-29 18:17:06 +0100

    seprateconcerns

--- anotherExperiment/api/models/companies.go
+++
@@ -1,9 +0,0 @@
-package models
-
-//todo handle uplaoding logo to db
-type COMPANY struct {
-	//Job_id      int       `json:"id,omitempty" db:"job_id"`
-	CompanyId int    `json:"company_id,omitempty" db:"company_id"`
-	Name      string `json:"name,omitempty" db:"name"`
-	Logo      string `json:"logo,omitempty" db:"logo"`
-}
--- anotherExperiment/api/models/jobs_description.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type JOB_DESCRIPTION struct {
-	JobId          int       `json:"job_id,omitempty" db:"job_id"`
-	JobDescription string    `json:"job_description,omitempty" db:"job_description"`
-	Embedding      []float32 // post add after first batch insert
-}
