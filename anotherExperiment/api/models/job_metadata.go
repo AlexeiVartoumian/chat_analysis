7228324cf6fd35ac9a96163131ba8f622f83d6fd
alex vartoumain <alex.vartoumian@gmail.com>
2026-03-29 18:37:47 +0100

    trim the fat

--- anotherExperiment/api/models/job_metadata.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Jobs_metadata struct {
-	JobId           int    `json:"job_id,omitempty" db:"job_id"`
-	ApplicantsCount string `json:"applicants_count,omitempty" db:"applicants_count"`
-	CompanyApplyUrl string `json:"company_apply_url,omitempty" db:"company_apply_url"`
-	JobState        string `json:"job_state,omitempty" db:"job_state"`
-}
