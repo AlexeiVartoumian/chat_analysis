70bb39f3252c0837ee6c769e3ce1cb198f141cbd
alex vartoumain <alex.vartoumian@gmail.com>
2026-04-02 18:59:02 +0100

    scalin

--- anotherExperiment/listbucket.py
+++ anotherExperiment/listbucket.py
@@ -3,18 +3,43 @@ import os
 
 client = boto3.client('s3')
 
+##check available files . pair off against local "seen" file . 
+#write download obj
 
 
-with open ("processedJobs.csv" , "wb" ) as f :
-    client.download_fileobj('alexeitranscribefile', 'input/processedJobs.csv', f)
+resp = client.list_objects(Bucket='alexeitranscribefile')
 
-with open("company_data.csv" , "wb" ) as f :
-    client.download_fileobj('alexeitranscribefile', '/output/company_data.csv', f)
+keys = []
+for key in resp["Contents"]:
+    print(key["Key"])
+    #print("ok \n")
+    keys.append(key)
+#print(resp)
 
-with open("job_metadata.csv" , "wb" ) as f :
-    client.download_fileobj('alexeitranscribefile', '/output/job_metadata.csv', f)
+for key in keys:
+    with open ("processedJobs.csv" , "wb" ) as f :
+        client.download_fileobj('alexeitranscribefile', key, f)
 
-with open("job_description.csv" , "wb" ) as f :
-    client.download_fileobj('alexeitranscribefile', '/output/job_description.csv', f)
+    with open("company_data.csv" , "wb" ) as f :
+        client.download_fileobj('alexeitranscribefile', key, f)
+
+    with open("job_metadata.csv" , "wb" ) as f :
+        client.download_fileobj('alexeitranscribefile', key, f)
+
+    with open("job_description.csv" , "wb" ) as f :
+        client.download_fileobj('alexeitranscribefile', key, f)
+
+
+# with open ("processedJobs.csv" , "wb" ) as f :
+#     client.download_fileobj('alexeitranscribefile', 'input/processedJobs.csv', f)
+
+# with open("company_data.csv" , "wb" ) as f :
+#     client.download_fileobj('alexeitranscribefile', '/output/company_data.csv', f)
+
+# with open("job_metadata.csv" , "wb" ) as f :
+#     client.download_fileobj('alexeitranscribefile', '/output/job_metadata.csv', f)
+
+# with open("job_description.csv" , "wb" ) as f :
+#     client.download_fileobj('alexeitranscribefile', '/output/job_description.csv', f)
 
 
