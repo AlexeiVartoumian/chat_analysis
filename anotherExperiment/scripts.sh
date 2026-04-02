78f2eb9327cfb9902c8f5456afd47fa7969c6583
alex vartoumain <alex.vartoumian@gmail.com>
2026-04-02 20:33:23 +0100

    deps

--- anotherExperiment/scripts.sh
+++ anotherExperiment/scripts.sh
@@ -10,7 +10,7 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] 
 
 
 sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io jq
 
 sudo systemctl start docker
 sudo usermod -aG docker $USER
