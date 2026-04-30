b82cba405d0bb49eca06e127f88e38ac2e55e7e7
alex vartoumain <alex.vartoumian@gmail.com>
2026-04-30 14:46:17 +0100

    j

--- anotherExperiment/listbucketv2.py
+++ anotherExperiment/listbucketv2.py
@@ -34,7 +34,7 @@ with open("keys.txt" , "r" , encoding="utf-8") as f:
         #print(line)
         seen_keys.add(line.strip("\n"))
 
-
+output_store_standard_timelines = set()
 for page in pages :
     for obj in page.get('Contents' , []):
         key = obj['Key']
@@ -53,7 +53,7 @@ for page in pages :
              
                 if year_month_date not in timelines:
                     timelines.append(year_month_date)
-                    
+                    output_store_standard_timelines.add(year_month_date)
                 new_keys_to_upload.append(check)
                 keys.append(key) ##will need to actually download
 
@@ -74,10 +74,16 @@ for page in pages2 :
                 base = os.path.basename(check)
                 if year_month_date not in timelines:
                     timelines.append(year_month_date)
-               
+
+                #a large scan should only happen once a day . otherwise this code block will break
+                if base.startswith("live"):
+                    timelinecheck[year_month_date].add("l")
+                    timelineindexcount.append(year_month_date)
+
                 if base.startswith("suspended"):
                     timelinecheck[year_month_date].add("s")
                     timelineindexcount.append(year_month_date)
+
                 if base.startswith("expired"):
                     timelinecheck[year_month_date].add("e")
                     timelineindexcount.append(year_month_date)
@@ -126,10 +132,19 @@ with open("keys.json" , "w" , encoding="utf-8" ) as f:
     for index , timeline in enumerate(timelines):
        
         standard_timeline =  datetime.strftime(timeline ,date_format)
-        output[standard_timeline] = [{}]
 
-        print("standard", standard_timeline,output[standard_timeline])
+        #it could be a standard job does not happen on a given day but a backfill one does
+        if timeline in output_store_standard_timelines:
+            output[standard_timeline] = [{}]
+
+            print("standard", standard_timeline,output[standard_timeline])
+            
         if index in backfill_timelineindexes:
+
+            if "l" in timelinecheck[timeline]:
+                
+                back_fill_timeline =  datetime.strftime(timeline ,date_format)
+                output[f"{back_fill_timeline}_liveRoles"] = [{}]
            
             #order of ops expiry workflow comes before suspend workflow atm can guarantee they appear once
             if "e" in timelinecheck[timeline]:
@@ -177,6 +192,12 @@ with open("keys.json" , "w" , encoding="utf-8" ) as f:
             #output[timeline_key][0][records[unique][3]] = document
             forgive(unique ,output[timeline_key][0])
             output[timeline_key][0][unique][3] = document
+
+        if document.startswith("live"):
+            
+        
+            dblforgive(unique ,output[f"{timeline_key}_liveRoles"][0])
+            output[f"{timeline_key}_liveRoles"][0][unique][0] = document
         
         if document.startswith("expired"):
             
@@ -191,18 +212,18 @@ with open("keys.json" , "w" , encoding="utf-8" ) as f:
     json.dump(output, f)
   
 
-for index ,key in enumerate(keys):
-    #print(key)
-    sanitizekey = new_keys[index]
-    # print(sanitizekey)
-    # print("here we go \n")
-    if "expired" in key  or "suspended" in key:
+# for index ,key in enumerate(keys):
+#     #print(key)
+#     sanitizekey = new_keys[index]
+#     # print(sanitizekey)
+#     # print("here we go \n")
+#     if key.startswith("suspended") or key.startswith("expired")  or key.startswith("live") :
         
-        with open (sanitizekey , "wb" ) as f :
-            client.download_fileobj('backfill-store-390746273208', key, f)
-        continue
-    with open (sanitizekey , "wb" ) as f :
-        client.download_fileobj('output-store-390746273208', key, f)
+#         with open (sanitizekey , "wb" ) as f :
+#             client.download_fileobj('backfill-store-390746273208', key, f)
+#         continue
+#     with open (sanitizekey , "wb" ) as f :
+#         client.download_fileobj('output-store-390746273208', key, f)
 
 
 
