b3d26e153599770882796ec19bc03c5affa2d6e1
alex vartoumain <alex.vartoumian@gmail.com>
2026-05-15 20:09:39 +0100

    commit

--- anotherExperiment/model.py
+++ anotherExperiment/model.py
@@ -197,6 +197,7 @@ def create_tables(conn) -> None:
                 first_seen_closed_at TIMESTAMPTZ,
                 next_scan_at         TIMESTAMPTZ,
                 suspended_count      INT,
+                visited              BOOLEAN         NOT NULL DEFAULT FALSE
                 CONSTRAINT fk_job_lifecycle_jobs
                     FOREIGN KEY (job_id)
                     REFERENCES JOBS (job_id)
