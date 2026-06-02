38649525f877e450628240249505c9051cf523c6
alex vartoumain <alex.vartoumian@gmail.com>
2026-06-02 21:40:46 +0100

    deed to the house

---
+++ anotherExperiment/iterdeedtest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+while IFS= read -r file; do
+    
+    echo "$file"
+    type="${file%%-*}"
+    type="${type^^}"
+
+    if [ "$type" = "PROCESSEDJOBSIND" ]; then 
+        ./start insert $file COMPANY_DEED
+        ./start insert $file JOBS_DEED
+    
+    elif [ "$type" = "JOBDESCRIPTIONIND"]; then
+        ./start insert $file JOB_DESCRIPTION_DEED
+    fi 
+
+done < <(jq -r '.[][]' keys_deed.json)
