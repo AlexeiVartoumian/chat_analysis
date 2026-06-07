4e23b55d44a03cf657bc6a3bd624d98af1a6e065
alex vartoumain <alex.vartoumian@gmail.com>
2026-06-07 17:20:32 +0100

    med

--- anotherExperiment/model.py
+++ anotherExperiment/model.py
@@ -305,13 +305,13 @@ def create_tables(conn) -> None:
 
         cur.execute("""
             CREATE TABLE IF NOT EXISTS JOB_LIFECYCLE_DEED (
-                job_id               BIGINT          PRIMARY KEY,
+                job_id               TEXT            PRIMARY KEY,
                 job_state            VARCHAR(256)    NOT NULL,
                 first_seen_at        TIMESTAMPTZ     NOT NULL DEFAULT NOW(),
                 last_seen_listed_at  TIMESTAMPTZ     NOT NULL DEFAULT NOW(),
                 first_seen_closed_at TIMESTAMPTZ,
                 next_scan_at         TIMESTAMPTZ,
-                visited              BOOLEAN         NOT NULL DEFAULT FALSE
+                visited              BOOLEAN         NOT NULL DEFAULT FALSE,
                 CONSTRAINT fk_job_lifecycle_jobs
                     FOREIGN KEY (job_id)
                     REFERENCES JOBS_DEED (job_id)
