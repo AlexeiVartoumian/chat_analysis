697ff099f1f5cef1024393519b958eb9996e1af9
alex vartoumain <alex.vartoumian@gmail.com>
2026-06-24 20:40:10 +0100

    null strings

--- anotherExperiment/cli/models/correlation.go
+++ anotherExperiment/cli/models/correlation.go
@@ -1,11 +1,13 @@
 package models
 
+import "database/sql"
+
 type CompanyCorrelation struct {
-	Company_link_id   int    `json:"company_id,omitempty" db:"company_link_id"`
-	Company_deed_id   string `json:"companydeed_id,omitempty" db:"company_deed_id"`
-	Company_name      string `json:"company_name,omitempty" db:"company_name"`
-	Company_link_url  string `json:"company_link_url,omitempty" db:"company_link_url"`
-	Company_deed_url  string `json:"company_deed_url,omitempty" db:"company_deed_url"`
-	Company_apply_url string `json:"company_apply_url,omitempty" db:"company_apply_url"`
-	JobDeed_url       string `json:"jobdeed_url,omitempty" db:"job_deed_url"`
+	Company_link_id   int            `json:"company_id,omitempty" db:"company_link_id"`
+	Company_deed_id   string         `json:"companydeed_id,omitempty" db:"company_deed_id"`
+	Company_name      string         `json:"company_name,omitempty" db:"company_name"`
+	Company_link_url  sql.NullString `json:"company_link_url,omitempty" db:"company_link_url"`
+	Company_deed_url  sql.NullString `json:"company_deed_url,omitempty" db:"company_deed_url"`
+	Company_apply_url sql.NullString `json:"company_apply_url,omitempty" db:"company_apply_url"`
+	JobDeed_url       sql.NullString `json:"jobdeed_url,omitempty" db:"job_deed_url"`
 }
