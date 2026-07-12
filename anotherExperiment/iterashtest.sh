651e24bb9881676305cd7bdc259ed043653de838
alex vartoumain <alex.vartoumian@gmail.com>
2026-07-12 16:29:47 +0200

    whoa

---
+++ anotherExperiment/iterashtest.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+while IFS= read -r file; do
+    
+    echo "$file"
+    type="${file%%-*}"
+    type="${type^^}"
+
+    if [ "$type" = "PROCESSEDJOBSASH" ]; then 
+        ./start insert $file COMPANY_ASH
+        ./start insert $file JOBS_ASH
+        ./start insert $file JOB_LIFECYCLE_ASH
+    
+    elif [ "$type" = "JOBDESCRIPTIONASH" ]; then
+        ./start insert $file JOB_DESCRIPTIONS_ASH
+    
+    fi 
+
+done < <(jq -r '.[][][][]' keys_deed.json)
