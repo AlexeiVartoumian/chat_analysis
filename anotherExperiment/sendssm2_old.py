68b7b4569b2e76b7fc9edf0a3d40023384e12679
alex vartoumain <alex.vartoumian@gmail.com>
2026-08-22 19:36:39 +0100

    ok

--- anotherExperiment/sendssm2_old.py
+++ anotherExperiment/sendssm2_old.py
@@ -104,37 +104,36 @@ def acquire_lock(workflow_id):
         raise
 
 scroller_count = 1
-file = None
 for term in search_terms:
 
     search_term_id = term["search_term_id"]
     search_term = term["search_term"]
 
-    # if first_run == "yes":
-    workflow_id = str(uuid.uuid4())
-    file = acquire_lock(workflow_id)
-    scroller_worker = f"scroller-{scroller_count}"
-    scroller_count+=1
-    response = ec2_client.run_instances(LaunchTemplate={'LaunchTemplateId': 'lt-0989cbde3348f9e83', 'Version': '$Latest'} ,MinCount=1,MaxCount=1, TagSpecifications=[{'ResourceType': 'instance','Tags': [{'Key': 'Name', 'Value': f'{scroller_worker}'}]}])
+    if first_run == "yes":
+        workflow_id = str(uuid.uuid4())
+        file = acquire_lock(workflow_id)
+        scroller_worker = f"scroller-{scroller_count}"
+        scroller_count+=1
+        response = ec2_client.run_instances(LaunchTemplate={'LaunchTemplateId': 'lt-0989cbde3348f9e83', 'Version': '$Latest'} ,MinCount=1,MaxCount=1, TagSpecifications=[{'ResourceType': 'instance','Tags': [{'Key': 'Name', 'Value': f'{scroller_worker}'}]}])
 
-    instance_id = response['Instances'][0]['InstanceId']
+        instance_id = response['Instances'][0]['InstanceId']
 
 
-    ssm_client = boto3.client('ssm', region_name="eu-west-2")
-    wait_for_instance_running(ec2_client, instance_id)
-    wait_for_ssm_online(ssm_client, instance_id)
-    
-    
+        ssm_client = boto3.client('ssm', region_name="eu-west-2")
+        wait_for_instance_running(ec2_client, instance_id)
+        wait_for_ssm_online(ssm_client, instance_id)
+        
+        
 
-    ssm_response = ssm_client.send_command(
-        InstanceIds=[instance_id],
-        DocumentName="AWS-RunShellScript",
-        Parameters={
-            "commands": [
-                f"sudo -u ubuntu bash -c 'export DISPLAY=:1 && export Instance_id={instance_id} && export S3_BUCKET=alexeitranscribefile && export PROFILE_S3_KEY={file['profile_id']} && cd /opt/myapp && /venv/bin/python3 extractprofile.py'"
-            ]
-        }
-    )
+        ssm_response = ssm_client.send_command(
+            InstanceIds=[instance_id],
+            DocumentName="AWS-RunShellScript",
+            Parameters={
+                "commands": [
+                    f"sudo -u ubuntu bash -c 'export DISPLAY=:1 && export Instance_id={instance_id} && export S3_BUCKET=alexeitranscribefile && export PROFILE_S3_KEY={file['profile_id']} && cd /opt/myapp && /venv/bin/python3 extractprofile.py'"
+                ]
+            }
+        )
 
 
 
@@ -151,22 +150,12 @@ for term in search_terms:
         inner_cmd = (
             f"export DISPLAY=:1 && "
             f"export workflow_id={shlex.quote(workflow_id)} && "
-            f"export profile_id={shlex.quote(file['profile_id'])} && "
             f"export search_term={shlex.quote(search_term)} && "
             f"export search_term_id={search_term_id} &&"
-            f"cd /opt/myapp && " 
-            f"timeout 1800 /venv/bin/python3 screenshot.py > /tmp/last_run.log 2>&1; echo EXIT_CODE:$?"
-            f"echo EXIT_CODE:$? > /tmp/exit_code.txt"
+            f"cd /opt/myapp && /venv/bin/python3 screenshot.py > /tmp/last_run.log 2>&1; echo EXIT_CODE:$?"
         )
 
         full_cmd = f"sudo -u ubuntu bash -c {shlex.quote(inner_cmd)}"
-        commands = [
-            full_cmd,
-            "cat /tmp/exit_code.txt",   
-            "sleep 15",                 # CloudWatch flush logs
-            "shutdown -h now",          
-        ]#want this to be self terminating
-
         ssm_response = ssm_client.send_command(
             InstanceIds=[instance_id],
             DocumentName="AWS-RunShellScript",
