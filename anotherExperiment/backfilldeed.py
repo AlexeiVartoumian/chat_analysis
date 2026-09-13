58d1f4871d3588538b8d9751c37a8fb6843e9866
alex vartoumain <alex.vartoumian@gmail.com>
2026-09-13 14:43:07 +0100

    a

--- anotherExperiment/backfilldeed.py
+++ anotherExperiment/backfilldeed.py
@@ -84,7 +84,7 @@ with open("newkeys_deed.txt" , "w" , encoding="utf-8") as f :
 def forgive(key , mydict ):
 
     if key not in mydict:
-        mydict[key] =  [0] *3    
+        mydict[key] =  [0] *4   
     return mydict
 
 def dblforgive(key , mydict ):
@@ -145,6 +145,10 @@ with open("keys_deed.json" , "w" , encoding="utf-8" ) as f:
         if document.startswith("deadlinks"):
             forgive(unique ,output[timeline_key][0])
             output[timeline_key][0][unique][2] = document
+
+        if document.startswith("jobdescription"):
+            forgive(unique ,output[timeline_key][0])
+            output[timeline_key][0][unique][3] = document
         
         
     json.dump(output, f)
